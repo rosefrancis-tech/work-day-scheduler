efcf511acb743f33c78da7aabb893852ae4b8e1f
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-11 15:11:28 -0500

    refactor styles of grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,28 @@ var hourOfTheDay = currentTime.format("ha");
 $(".currentDay").text(currentTime.format("dddd, MMMM Do"));
 
 
-
-
+var index = 0;
 // style time-blocks according to the present, past and future times
 $(".row").find($(".hour")).each(function() {
-    var schedulerTime = $(this).find($(".time-block")).text().trim().toLowerCase();
+    // time displayed in html
+    var schedulerTimeText = $(this).find($(".time-block")).text().trim().toLowerCase();
+    // convert the scheduler time from string to moment
+    var schedulerTime = moment(schedulerTimeText,'ha');
+    // select the row for applying styles
+    var eventEl = $(".row").find($(".description")).eq(index);
 
-    if(schedulerTime === hourOfTheDay) {
-        $(".row").each(function() {
-            $(this).find($(".row")).addClass("present");
-        })
+    index++;
+    // conditions for comparing current time and scheduler time
+    if(moment().isAfter(schedulerTime)) {
+        eventEl.addClass("past");
     }
-
-    if(schedulerTime < hourOfTheDay) {
-        $(".row").each(function() {
-            $(this).find($(".row")).addClass("past");
-        })
+    if(moment().isBefore(schedulerTime)) {
+        eventEl.addClass("future");
     }
-
-    if(schedulerTime > hourOfTheDay) {
-        $(".row").each(function() {
-            $(this).find($(".row")).addClass("future");
-        })
+    if(hourOfTheDay === schedulerTimeText) {
+        eventEl.addClass("present");
     }
-
 })
 
+
+// edit events
