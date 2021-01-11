135c4ed8a8a275ed4a49ec45c2e3ad256b17be67
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-11 02:34:34 -0500

    style events dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,34 @@
+// Current Time
+var currentTime = moment();
+var hourOfTheDay = currentTime.format("ha");
+
 // display today's date in header
-var currentTime = moment().format("dddd, MMMM Do");
-$(".currentDay").text(currentTime);
+$(".currentDay").text(currentTime.format("dddd, MMMM Do"));
+
+
+
+
+// style time-blocks according to the present, past and future times
+$(".row").find($(".hour")).each(function() {
+    var schedulerTime = $(this).find($(".time-block")).text().trim().toLowerCase();
+
+    if(schedulerTime === hourOfTheDay) {
+        $(".row").each(function() {
+            $(this).find($(".row")).addClass("present");
+        })
+    }
+
+    if(schedulerTime < hourOfTheDay) {
+        $(".row").each(function() {
+            $(this).find($(".row")).addClass("past");
+        })
+    }
+
+    if(schedulerTime > hourOfTheDay) {
+        $(".row").each(function() {
+            $(this).find($(".row")).addClass("future");
+        })
+    }
+
+})
 
