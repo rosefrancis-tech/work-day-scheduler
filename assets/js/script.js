c6d40ca8e361c72afdd4c78c774a9918462b0fcd
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-12 16:07:34 -0500

    blur textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var currentTime = moment();
 var hourOfTheDay = currentTime.format("ha");
 
+var editedText = "";
+
 // display today's date in header
 $(".currentDay").text(currentTime.format("dddd, MMMM Do"));
 
@@ -29,23 +31,49 @@ $(".row").find($(".hour")).each(function() {
 })
 
 // edit events
-$(".description").on("click", function(){
+$(".description").on("click", function() {
+    //debugger;
     // get current text 
-    var text = $(this)
+    var texts = $(this)
     .text()
     .trim();
     // get current class
     var curClass = $(this)
     .attr("class");
 
-    // replace colomn element with a new textarea
-    var textInput = $("<textarea>").addClass(curClass).val(text);
+    // replace column element with a new textarea
+    var textInput = $("<textarea>").addClass(curClass).val(texts);
     $(this).replaceWith(textInput);
 
     // auto focus new element
     textInput.trigger("focus");
-    });
+})
 
+// return to div from text area
+$(".description").on("blur", function() {
+    //debugger;
+    // get current text 
+    var texts = $("<textarea>").val();
+    // get current class
+    var curClass = $("<textarea>")
+    .attr("class");
+    
+    // recreate the div element
+    var textDisplay = $("<p>").addClass(curClass).text(texts);
+    $("<textarea>").replaceWith(textDisplay);
 
+    // update events in local storage
 
 })
+
+$(".container").on("click", "i", function(){
+    
+    editedText = $(".description").value;
+    console.log(editedText);
+    // add events in an array
+
+    events = { task: editedText };
+    // save to local storage
+    localStorage.setItem("events", JSON.stringify(events));
+})
+
