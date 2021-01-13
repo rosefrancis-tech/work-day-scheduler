4292432f67c86071d325f9cd7bf407a229ec8e2e
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-13 02:12:10 -0500

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var currentTime = moment();
 var hourOfTheDay = currentTime.format("ha");
 
 var editedText = "";
+var events = [];
+var eventObj = {};
 
 // display today's date in header
 $(".currentDay").text(currentTime.format("dddd, MMMM Do"));
@@ -46,45 +48,58 @@ $(".description").on("click","p", function() {
     textInput.trigger("focus");
 })
 
-// return to div from text area
+// unfocus and save if away from editing
 $(".description").on("blur","textarea", function() {
     //debugger;
     // get current text 
     var texts = $(this).val();
+    var currentTimeBlock = $(this)
+                            .closest(".description")
+                            .prev(".hour")
+                            .children(".time-block")
+                            .text()
+                            .trim()
+                            .toLowerCase();
+    
+    console.log(currentTimeBlock);
+
+    // add current edited event and corresponding time-block as object
+    eventObj = {
+        timeBlock: currentTimeBlock,
+        textBlock: texts
+    }
+
+    
+
+    console.log(eventObj);
      
     // recreate the div element
     var textDisplay = $("<p>").addClass("text-block").text(texts);
     $(this).replaceWith(textDisplay);
 
-    // update events in local storage
-    
+// save
+saveTasks();
 
 })
-var events = {};
+
 
 var loadevents = function() {
 
     tasks = JSON.parse(localStorage.getItem("events"));
     // if nothing in localStorage, create a new object to track all task status arrays
     if (!events) {
-      events = {events:[]};
+      events.push(eventObj);
     }
 };
 
 var saveTasks = function() {
-
+    events.push(eventObj);
    // save to local storage
     localStorage.setItem("events", JSON.stringify(events));
 };
 
 $(".container").on("click", "i", function(){
-    
-    editedText = $(".description").text;
-    console.log(editedText);
-    // add events in an array
-
-    events = { task: editedText };
-    // save to local storage
-    localStorage.setItem("events", JSON.stringify(events));
+    // 
+    saveTasks();
 })
 
