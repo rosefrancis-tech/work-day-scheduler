e60deb34907cdd316391c6214a31cf23f369541f
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-14 00:10:40 -0500

    rearrange codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,20 @@ var events = [];
 var currentEvents = [];
 var eventObj = {};
 
-
-// display today's date in header
+// Display today's date in header
 $(".currentDay").text(currentTime.format("dddd, MMMM Do"));
 
-// INITIAL LOAD events and appropriate color coding
+// INITIAL load events and appropriate color coding
 var index1 = 0;
-// style time-blocks according to the present, past and future times
+// Style time-blocks according to the present, past and future times
 $(".row").find($(".hour")).each(function() {
+    
     // time displayed in html
     var schedulerTimeText = $(this).find($(".time-block")).text().trim().toLowerCase();
     // convert the scheduler time from string to moment
     var schedulerTime = moment(schedulerTimeText,'ha');
     // select the row for applying styles
     var eventEl = $(".row").find($(".description")).eq(index1);
-
     index1++;
     // conditions for comparing current time and scheduler time
     if(moment().isAfter(schedulerTime)) {
@@ -31,7 +30,7 @@ $(".row").find($(".hour")).each(function() {
     if(hourOfTheDay === schedulerTimeText) {
         eventEl.addClass("present").removeClass("past");
     }
-
+    // load saved events to intial page
     currentEvents = JSON.parse(localStorage.getItem("events"));
     $.each(currentEvents, function(index,value) {
         if(schedulerTimeText === value.timeBlock) {
@@ -41,51 +40,34 @@ $(".row").find($(".hour")).each(function() {
 
 })
 
-// edit events
+// Function for edit events on click
 $(".description").on("click","p", function() {
     
     // get current text 
     var texts = $(this)
     .text()
     .trim();
-
-    // replace column element with a new textarea
+    // replace p element with a new textarea
     var textInput = $("<textarea>").addClass("text-block").val(texts);
     $(this).replaceWith(textInput);
-
     // auto focus new element
     textInput.trigger("focus");
 });
 
-// unfocus and save if away from editing
+// Function for unfocus and show as not saved if away from editing
 $(".description").on("blur","textarea", function() {
     
     // get current text 
     var texts = $(this).val();
-    //console.log($(this));
-    /*
-    var currentTimeBlock = $(this)
-                            .closest(".description")
-                            .prev(".hour")
-                            .children(".time-block")
-                            .text()
-                            .trim()
-                            .toLowerCase();
-    // add current edited event and corresponding time-block as object
-    eventObj = {
-        timeBlock: currentTimeBlock,
-        textBlock: texts
-    } */
-    // recreate the p element, show as not saved
+    // remove textarea, recreate the p element, show as not saved
     var textDisplay = $("<p>").addClass("text-block not-saved").text(texts);
     $(this).replaceWith(textDisplay);
 });
 
-// save to local storage when save icon is clicked
+// Function for save-icon click
 $(".row").on("click", "i", function(){
-  
-      // debugger;
-        console.log($(this));
+    
+    // get the values of current event text and corresponding time-block
     var currentTimeBlock = $(this).closest(".row").find($(".time-block")).text().trim().toLowerCase();
     var currentTextBlock = $(this)
         .closest(".row")
@@ -93,45 +75,40 @@ $(".row").on("click", "i", function(){
         .text().trim();
         console.log(currentTextBlock);
         console.log(currentTimeBlock);
-    // add current edited event and corresponding time-block as object
+    // keep current edited event text and corresponding time-block as object
     eventObj = {
         timeBlock: currentTimeBlock,
         textBlock: currentTextBlock
     };
-    //debugger;
-    console.log("eventobj" + eventObj);
+    // get events from local storage
     currentEvents = JSON.parse(localStorage.getItem("events"));
-    console.log("storage" +currentEvents);
+    // condition for any values in local storage
     if(currentEvents != null) {
         for(var j=0; j < currentEvents.length; j++){
             // if local storage has values for same timeblock, then remove it
             if(currentEvents[j].timeBlock === eventObj.timeBlock) {
                 currentEvents.splice(j,1);
-                console.log("splice storage" +currentEvents);
                 break;
             }
         }
         currentEvents.push(eventObj);
-        console.log("after push" + currentEvents);
         events = currentEvents;
-        console.log("events with storage" + events);
     }
     else {
-        events.push(eventObj); 
-        console.log("events without storage" + events);    
+        events.push(eventObj);     
     }
     // remove dotted border on saving
     $(this).closest(".saveBtn").prev(".description").children(".text-block").removeClass("not-saved");
     saveTasks();
-})
+});
 
-// function for clicking clear all icon
+// Function for clicking Trash icon
 $(".clear-all").on("click", function(){
     localStorage.clear();
     location.reload();
-})
+});
 
-// function for saving to local storage
+// Function for saving to local storage
 var saveTasks = function() {  
     localStorage.setItem("events", JSON.stringify(events));
 };
