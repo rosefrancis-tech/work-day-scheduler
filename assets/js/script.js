d2ecc50cab73caec58b658b5f243e73c13330de8
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-13 19:20:24 -0500

    enable save for corresponding textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,13 +55,15 @@ $(".description").on("click","p", function() {
 
     // auto focus new element
     textInput.trigger("focus");
-})
+});
 
 // unfocus and save if away from editing
 $(".description").on("blur","textarea", function() {
     
     // get current text 
     var texts = $(this).val();
+    //console.log($(this));
+    /*
     var currentTimeBlock = $(this)
                             .closest(".description")
                             .prev(".hour")
@@ -73,31 +75,52 @@ $(".description").on("blur","textarea", function() {
     eventObj = {
         timeBlock: currentTimeBlock,
         textBlock: texts
-    } 
+    } */
     // recreate the p element, show as not saved
     var textDisplay = $("<p>").addClass("text-block not-saved").text(texts);
     $(this).replaceWith(textDisplay);
-})
+});
 
 // save to local storage when save icon is clicked
-$(".container").on("click", "i", function(){
+$(".row").on("click", "i", function(){
+  
+      // debugger;
+        console.log($(this));
+    var currentTimeBlock = $(this).closest(".row").find($(".time-block")).text().trim().toLowerCase();
+    var currentTextBlock = $(this)
+        .closest(".row")
+        .find($(".text-block"))
+        .text().trim();
+        console.log(currentTextBlock);
+        console.log(currentTimeBlock);
+    // add current edited event and corresponding time-block as object
+    eventObj = {
+        timeBlock: currentTimeBlock,
+        textBlock: currentTextBlock
+    };
+    //debugger;
+    console.log("eventobj" + eventObj);
     currentEvents = JSON.parse(localStorage.getItem("events"));
-    
+    console.log("storage" +currentEvents);
     if(currentEvents != null) {
         for(var j=0; j < currentEvents.length; j++){
             // if local storage has values for same timeblock, then remove it
             if(currentEvents[j].timeBlock === eventObj.timeBlock) {
                 currentEvents.splice(j,1);
+                console.log("splice storage" +currentEvents);
                 break;
             }
         }
-        
         currentEvents.push(eventObj);
+        console.log("after push" + currentEvents);
         events = currentEvents;
+        console.log("events with storage" + events);
     }
     else {
-        events.push(eventObj);     
+        events.push(eventObj); 
+        console.log("events without storage" + events);    
     }
+    // remove dotted border on saving
     $(this).closest(".saveBtn").prev(".description").children(".text-block").removeClass("not-saved");
     saveTasks();
 })
